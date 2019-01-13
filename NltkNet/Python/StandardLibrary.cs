fa51f144ad73d5efdbc9d9f612ae5ff047b29890
Denis K <den.kasakov@gmail.com>
2019-01-13 12:51:16 +0200

    Zip built-in function wrapper

--- NltkNet/Python/StandardLibrary.cs
+++ NltkNet/Python/StandardLibrary.cs
@@ -17,7 +17,8 @@ namespace NltkNet
                                   "def __set__(obj):\r\n\treturn set(obj)\r\n" +
                                   "def __list__(obj):\r\n\treturn list(obj)\r\n" +
                                   "def __sorted__(obj):\r\n\treturn sorted(obj)\r\n" +
-                                  "def __range__(p1, p2, p3):\r\n\treturn range(start=p1, stop=p2, step=p3)\r\n"
+                                  "def __range__(p1, p2, p3):\r\n\treturn range(start=p1, stop=p2, step=p3)\r\n" +
+                                  "def __zip__(*obj):\r\n\treturn zip(*obj)\r\n"
                                   );
         }
 
@@ -28,19 +29,25 @@ namespace NltkNet
         public static dynamic List(dynamic pyObj) => Nltk.Py.CallFunction("__list__", pyObj);
         public static dynamic Sorted(dynamic pyObj) => Nltk.Py.CallFunction("__sorted__", pyObj);
         public static dynamic Range(int start, int stop, int step = 1) => Nltk.Py.CallFunction("__range__", start, stop, step);
+        public static dynamic Zip(params dynamic[] objects) =>  Nltk.Py.CallFunction("__zip__", objects);        
 
 
         public static void InternalTest()
         {
-            //var lst = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
+            var lst = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
+            //var lst2 = new List<int>() { 10, 20, 30, 40, 5, 1, 2, 3, 4, 5 };
+            var lst2 = new List<string>() { "A", "B", "C" };
+
             //Print("Len: " + Len(lst));
             //Print("Sorted: " + Str(Sorted(lst)));
             //var tuple = (1, 2, "str");
             //Print("Tuple2List: " + tuple);
             //Print(List(tuple));
             //Print("Set: " + Str(Set(lst)));
-            var range = Range(0, 30, 3);
-            Print("Range: " + Str(List(range)));
+            //var range = Range(0, 30, 3);
+            //Print("Range: " + Str(List(range)));
+
+            Print("Zip: " + Str(Zip(lst, lst2)));
         }
     }
 }
